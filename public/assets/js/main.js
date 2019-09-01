bc313d123972fc670011fd45d9a9f52d81473e65
michael mariea <mariea.michael@gmail.com>
2019-08-31 17:20:09 -0700

    swithed to a more restful use of api

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,57 +1,15 @@
 $(document).ready(function () {
     //click function
-    
-    let restaurantsArr= [];
-    $("#Btn").click(function (e) { 
-        console.log("click!");
-        e.preventDefault();
-        let location = $("#location").val();
-        const apikey = "0eb0881917e5b1d57a0d1780cef91180"
-        const url = "https://developers.zomato.com/api/v2.1/locations?query="+ location+"&apikey="+ apikey;
-        $.ajax({
-            url:url,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            let id = response.location_suggestions[0].entity_id;
-            let type = response.location_suggestions[0].entity_type;
-            const url2 = "https://developers.zomato.com/api/v2.1/location_details?entity_id=" + id + "&entity_type=" + type + "&apikey="+ apikey
-           console.log(id);
-           console.log(type);
-            $.ajax({
-                url: url2,
-                method:"GET"
-            }).then(function (response) {
-                console.log(response.best_rated_restaurant);
-                for (let i = 0; i < response.best_rated_restaurant.length; i++){
-                    console.log("hi");
-                    let r = response.best_rated_restaurant[i];
-                    let name = response.best_rated_restaurant[i].restaurant.name;
-                    let adress = response.best_rated_restaurant[i].restaurant.location.address;
-                    // console.log(name);
-                    // console.log(adress);
-                    
-                    let restaurant = {
-                        name: name,
-                        address: adress
-                    }
-
-                    restaurantsArr.push(restaurant);
-                    // console.log(restaurantsArry);
-                }
-                console.log(restaurantsArr);
-                $.ajax("/api/restaurant/location", {
-                    type: "POST",
-                    data:{restaurantsArr}
-                }).then(res => {
-                    
-                    // console.log(restaurantsArry.length);
-                    console.log(res)
-                });
-            });
-        });
-    });
+$("#Btn").click(function (e) {
+    e.preventDefault();
+    console.log("click!");
+    let location = $("#location").val();
+    $.ajax("/api/restaurant/location", {
+        type: "POST",
+        data:{location}
+        })
 })
+//nav bar movement
 $(function () {
     $('.nav-expand').click(e => {
         e.preventDefault();
@@ -60,4 +18,5 @@ $(function () {
     
         $('.nav-container').toggleClass('full-circle');
     });
+});
 });
--- server.js
+++ server.js
@@ -26,9 +26,73 @@ app.get("/contact", function (req, res) {
   res.render(path.join(__dirname, "/views/contact"));
 });
 
+
+
+app.get("/restaurants", function (req, res){
+  res.render(path.join(__dirname, "/views/carousel"));
+});
+
+
 app.post("/api/restaurant/location", function (req, res) {
-  console.log(req.body.restaurantsArr);
-  res.send("I DID IT");
+  console.log("working")
+  let restaurantsArr= [];
+  let location = req.body;
+  const apikey = "0eb0881917e5b1d57a0d1780cef91180"
+  const url = "https://developers.zomato.com/api/v2.1/locations?query="+ location+"&apikey="+ apikey;
+  $.ajax({
+    url:url,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    let id = response.location_suggestions[0].entity_id;
+    let type = response.location_suggestions[0].entity_type;
+    const url2 = "https://developers.zomato.com/api/v2.1/location_details?entity_id=" + id + "&entity_type=" + type + "&apikey="+ apikey
+    console.log(id);
+    console.log(type);
+    $.ajax({
+      url: url2,
+      method:"GET"
+    }).then(function (response) {
+      console.log(response.best_rated_restaurant);
+      for (let i = 0; i < response.best_rated_restaurant.length; i++){
+        console.log("hi");
+        let r = response.best_rated_restaurant[i];
+        let name = response.best_rated_restaurant[i].restaurant.name;
+        let adress = response.best_rated_restaurant[i].restaurant.location.address;
+        let phoneNum = response.best_rated_restaurant[i].restaurant.phone_numbers;
+        let pic = response.best_rated_restaurant[i].restaurant.photos[0].photo.url;
+        let hours = response.best_rated_restaurant[i].restaurant.timings;
+        let rating = response.best_rated_restaurant[i].restaurant.user_rating.aggregate_rating;
+        let votes = response.best_rated_restaurant[i].restaurant.user_rating.votes;
+        // console.log(name);
+        // console.log(adress);
+        
+        let restaurant = {
+          name: name,
+          address: adress,
+          phoneNum: phoneNum,
+          pic: pic,
+          hours: hours,
+          rating: rating,
+          votes: votes
+          
+        }
+        
+        restaurantsArr.push(restaurant);
+        // console.log(restaurantsArry);
+      }
+      console.log(restaurantsArr);
+      
+      
+      
+      
+      // res.render("/views/carousel", {restaurantsArr: req.body.restaurantsArr});
+    });
+  });
+  // res.send(req.body.restaurantsArr);
+  
+  // res.render(path.join(__dirname, "/views/carousel"));
+  // res.send("I DID IT");
   // res.render(path.join(__dirname, "/views/index.handlebars"));
   //ajax call using req.params
   //app.post to server from front end
@@ -38,6 +102,6 @@ app.post("/api/restaurant/location", function (req, res) {
 
 //listening on ***
 app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
   });
