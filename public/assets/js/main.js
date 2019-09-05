ab30331dcb207ae42730b73aa09fddf15e80c400
Sean A <1996.sean.alexander@gmail.com>
2019-09-05 16:15:09 -0700

    syned db to front end; EMOJI COUNT CAN NOW BE UPDATED

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,35 +1,4 @@
 $(function() {
-    // const emojis = {
-    //     tearsOfJoy: "&#x1F602;",
-    //     openMouthSmile: "&#x1F604;",
-    //     wink: "&#x1F609;",
-    //     blushSmile: "&#x1F60A;",
-    //     tongueOut: "&#x1F60B;",
-    //     sunglassesSmile: "&#x1F60E;",
-    //     heartEyes: "&#x1F60D;",
-    //     normalSmile: "&#x1F642;",
-    //     thinking: "&#x1F914;",
-    //     neutral: "&#x1F610;",
-    //     rollingEyes: "&#x1F644;",
-    //     smirk: "&#x1F60F;",
-    //     sleepy: "&#x1F62A;",
-    //     drooling: "&#x1F924;",
-    //     astonished: "&#x1F632;",
-    //     confused: "&#x1F615;",
-    //     confounded: "&#x1F616;",
-    //     disappointed: "&#x1F61E;",
-    //     worried: "&#x1F61F;",
-    //     loudlyCrying: "&#x1F62D;",
-    //     furious: "&#x1F621;",
-    //     lying: "&#x1F925;",
-    //     sickFace: "&#x1F922;",
-    //     poop: "&#x1F4A9;",
-    //     thumbsUp: "&#x1F44D;",
-    //     thumbsDown: "&#x1F44E;",
-    //     firstPlace: "&#x1F947;",
-    //     secondPlace: "&#x1F948;",
-    //     thirdPlace: "&#x1F949;"
-    // };
 
     let catagories = [
         "Breakfast",
@@ -68,16 +37,46 @@ $(function() {
         "Food Court",
         "Hawaiian"
     ];
-    // for (let i = 0; i < catagories.length; i++){
-    //     emoji = catagories[i];
-    //     $(".mdi-menu-down").append(emoji);
-    // }
-
 
     // EXPAND /RETRACT MENU
     $(".nav-expand").click(e => {
         e.preventDefault();
         $(".nav-container").toggleClass("full-circle");
+    });
+
+    $('.reactions').click(function (e) { 
+        e.preventDefault();
+        if ($(this).data('clicked') === false) {
+            const emojiName = $(this).attr("id");
+            const index = $(this).data("index");
+            const id = $(this).data("id");
+            const countContainer = $(this).children(".reactions__count");
+            let count = parseInt(countContainer.text());
+            count++;
+            console.log(`Count: ${count}`);
+            console.log(`Emoji Name: ${emojiName}`);
+            console.log(`Index: ${index}`);
+            $(this).data('clicked', true);
+            console.log($(this).data("clicked"));
+            const emojiData = {
+                name: emojiName,
+                index: index,
+                count: count,
+                id: id
+            }
+
+            $.ajax({
+                method: "PUT",
+                url: "/api/countUpdate",
+                data: emojiData
+            }).then(() => {
+                console.log("it's been updated!");
+                countContainer.text(count);
+
+            })
+            // $.put("/api/countUpdate", emojiData, () => {
+            // })
+        }
     });
 
     //initialize swiper when document ready
@@ -91,18 +90,16 @@ $(function() {
         resistence: false,
         resistenceRatio: 0.5,
         grabCursor: true,
+
         //   // Navigation arrows
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev"
         }
-        //   // And if we need scrollbar
-        //   scrollbar: {
-        //     el: '.swiper-scrollbar',
-        //   }
     });
 
     // HANDLE SUBMITTING LOCATION DATA EVENT
+
     $(".search__form").on("submit", function(e) {
         e.preventDefault();
         getLocation(
@@ -112,26 +109,19 @@ $(function() {
         );
     });
 
+    $('.arrow-btn').click(function (e) { 
+        let btn = $(this);
+        btn.addClass('rotate');
+
+        setTimeout(() => {
+            btn.removeClass('rotate');
+        }, 1000);
+    });
+
 });
+
 function getLocation(location) {
-    // $.get(`/api/location/${location}`, data => {
-    //     let businesses = data.detailsArr;
-    //     console.log(JSON.stringify(businesses));
-        window.location.href = `/app/${location}`;
-    // });
+    window.location.href = `/app/${location}`;
 }
 
-// function showEmojies(emojiObj) {
-//     let i = 0;
-//     let emojiHexCode = Object.values(emojiObj);
-//     let emojiNames = Object.keys(emojiObj);
-//     // console.log(emojiHexCode);
-//     // console.log(emojiNames);
-//     let $container = $('.reactions--container .simplebar-content');
-//     emojiHexCode.forEach(emoji => {
-//         let $reaction = $('<div>').addClass('reactions').html(emoji);
-//         let $reactionCount = $('<p>').addClass('reactions__count').html('100');
-//         $reaction.append($reactionCount);
-//         $container.append($reaction);
-//     });
-// }
+
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,4 +1,3 @@
-const path = require("path");
 const axios = require("axios");
 require("dotenv").config();
 const db = require("../models");
@@ -40,42 +39,12 @@ module.exports = app => {
         firstPlace: "🥇",
         secondPlace: "🥈",
         thirdPlace: "🥉"
-      };
+    };
 
-    //   const emojis = [
-    //       {
-    //           name: cjnjcvnd,
-    //           hex: jefnvj,
-    //           count: 0
-    //       },
-    //       {
-    //         name: cjnjcvnd,
-    //         hex: jefnvj,
-    //         count: 0
-    //     },
-    //     {
-    //         name: cjnjcvnd,
-    //         hex: jefnvj,
-    //         count: 0
-    //     }
-    //   ]
-
-      const emojiHex = Object.values(emojiName_Hex);
-
-    // APP PAGE
-    // app.get("/app/:location", function(req, res) {
-    //     // res.render(path.join(__dirname, "../views/app"));
-    //     // res.render("app", );
-    //     console.log(req.params.location);
-    //     console.log("This is before calling Yelp");
-    //     axios.get(`/api/location/${req.params.location}`).then(data => {
-    //         res.render("app", data.detailsArr);
-    //     });
-    // });
+    const emojiHex = Object.values(emojiName_Hex);
 
     app.get("/app/:location", (req, res) => {
         let location = req.params.location;
-        // console.log(location);
         location = location.replace(/ /g, "+");
         let detailsArr = [];
         const days = [
@@ -92,7 +61,6 @@ module.exports = app => {
             .get(
                 `https://api.yelp.com/v3/businesses/search?location=${location}&categories=food,bars,restaurants&limit=5`,
                 {
-                    // &categories=food,bars,restaurants&radius=30000
                     headers: {
                         Authorization: `Bearer ${process.env.API_KEY}`
                     }
@@ -101,6 +69,7 @@ module.exports = app => {
             .then(response => {
                 const businesses = response.data.businesses;
                 let k = 0;
+
                 //LOOP THROUGH ALL BUSINESSES & MAKE 2ND CALL TO YELP TO GET BUSINESS DETAILS FOR EACH
                 businesses.forEach(business => {
                     axios
@@ -115,8 +84,6 @@ module.exports = app => {
                         .then(response => {
                             let details;
                             let emojiKeys, emojiValues;
-                            // console.log(business.id);
-                            // console.log(JSON.stringify(hoursArr, null, 4));
                             const data = response.data;
                             db.Restaurant.findOrCreate({
                                 where: {
@@ -136,8 +103,6 @@ module.exports = app => {
                                     return [emojiKeys, emojiValues];
                                 })
                                 .then(emojis => {
-                                    console.log(emojis);
-
                                     let emojiArr = [];
                                     for (let i = 0; i < emojiHex.length; i++) {
                                         emojiArr.push({
@@ -147,12 +112,13 @@ module.exports = app => {
                                         });
                                     }
 
-
                                     details = {
+                                        id: business.id,
                                         name: data.name,
                                         images: data.photos,
                                         categories: data.categories,
                                         rating: data.rating,
+                                        price: data.price,
                                         address: data.location.display_address,
                                         url: data.url,
                                         phone: data.display_phone,
@@ -176,36 +142,25 @@ module.exports = app => {
                                         details.hours[days[i]] = {
                                             day: i,
                                             start: start,
-                                            end: end,
-                                            is_overnight:
-                                                data.hours[0].open[i]
-                                                    .is_overnight
+                                            end: end
                                         };
                                     }
-                                    if (!details.hours[days[6]]) {
-                                        console.log(
-                                            "This is where sunday is closed"
-                                        );
-                                        details.hours[days[6]] = {
-                                            day: 6,
-                                            start: "closed",
-                                            end: null,
-                                            is_overnight: null
-                                        };
+                                    for (let i = 0; i <= 6; i++) {
+                                        if (!details.hours[days[i]]) {
+                                            details.hours[days[i]] = {
+                                                day: i,
+                                                start: "closed",
+                                                end: null
+                                            };
+                                        }
                                     }
+
                                     detailsArr.push(details);
-                                    console.log(details);
                                     k++;
                                     if (k === businesses.length) {
-                                        console.log("made it in!");
-                                        // console.log(`
-                                        // -------------------------------------------------------------------------------------------------
-                                        // BELOW IS THE FULL DETAILS ARRAY BEING PUSHED TO THE FRONT END`);
-                                        // console.log(JSON.stringify(detailsArr, null, 4));
                                         res.render("app", {
                                             businesses: detailsArr
                                         });
-                                        // res.json(detailsArr);
                                     }
                                 });
                         })
@@ -213,8 +168,6 @@ module.exports = app => {
                             if (err) throw err;
                         });
                 });
-                console.log(`
-                ---------------------------------------------------------------------------------------------------`);
             })
             .catch(err => {
                 if (err) throw err;
